f058225aaccb546ebe2a47a9c850ca514bf3e376
amin <46023393+amin76hosseini@users.noreply.github.com>
2018-12-20 22:06:38 +0330

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,21 @@
+from asciimatics.effects import Cycle,Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+
+
+def demo(screen):
+    effects=[
+        Cycle(
+            screen,
+            FigletText("Hello",font='big'),
+            int(screen.height/2-8)),
+        Cycle(
+            screen,
+            FigletText("Happy Yalda",font='big'),
+            int(screen.height/2)),
+        Stars(screen,400)
+        ]
+    screen.play([Scene(effects,500)])
+
+Screen.wrapper(demo)
